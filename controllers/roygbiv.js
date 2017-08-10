4b20cac7197d7f1926842b7bdc209af0ca8ad008
Cathleen Wright <cathleenmwright@gmail.com>
2017-08-10 08:11:54 -0400

    Lists roygbiv colors on index page upon creation

--- controllers/roygbiv.js
+++ controllers/roygbiv.js
@@ -5,7 +5,11 @@ const Roygbiv 		= require('../models/roygbiv.js');
 
 //Restful Routes
 router.get('/', (req, res)=>{
-	res.render('roygbiv/index.ejs');
+	Roygbiv.find({}, (err, foundColors) => {
+		res.render('roygbiv/index.ejs', {
+			colors: foundColors
+		});
+	})
 });
 
 router.get('/create', (req, res)=>{
