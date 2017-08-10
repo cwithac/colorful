3738732de98caff91f07249157ee20cc8a8e2798
Cathleen Wright <cathleenmwright@gmail.com>
2017-08-10 07:38:53 -0400

    Connected CSS

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express   = require('express');
 const app       = express();
 const port      = 3000;
 
+// Middleware
+app.use(express.static('public'));
+
 //Restful Routes
 app.get('/', (req, res) => {
 	res.render('index.ejs');
