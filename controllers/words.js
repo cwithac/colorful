f8bc91b75ebd4b27334918a4357a16db1f97dcf5
Cathleen Wright <cathleenmwright@gmail.com>
2017-08-12 10:05:24 -0400

    Words index page with posting through create

--- controllers/words.js
+++ controllers/words.js
@@ -1,10 +1,25 @@
 //Global Variables
 const express 	= require('express');
 const router 		= express.Router();
+const Word      = require('../models/words.js');
 
 //Restful Routes
 router.get('/', (req, res) => {
-  res.render('words/index.ejs');
+  Word.find({}, (err, foundWords)=> {
+    res.render('words/index.ejs', {
+      words: foundWords
+    });
+  });
+});
+
+router.get('/create', (req, res) => {
+  res.render('words/create.ejs');
+});
+
+router.post('/', (req, res) => {
+  Word.create(req.body, (err, createdWord) => {
+    res.redirect('/words')
+  });
 });
 
 
---
+++ models/words.js
@@ -0,0 +1,12 @@
+//Global Variables
+const mongoose = require('mongoose');
+
+//Schema
+const wordSchema = mongoose.Schema({
+	name: String,
+	hex: String,
+});
+
+//Listeners
+const Word = mongoose.model('Word', wordSchema);
+module.exports = Word;
