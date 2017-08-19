cf8e988330d4675933327339cea200ee5fde3b3c
Cathleen Wright <cathleenmwright@gmail.com>
2017-08-19 11:27:39 -0400

    Seed data update

--- models/colorseed.js
+++ models/colorseed.js
@@ -1,24 +1,34 @@
 module.exports = [
   {
-    name: "Blue",
-    hex: '#0074D9',
-    description: 'blue is...'
-  },
-  {
-    name: "Yellow",
-    hex: '#FFDC00',
-    description: 'yellow is...'
-  },
-  {
-    name: "Green",
-    hex: '#2ECC40',
-    description: 'green is...'
-  },
-  {
-    name: "Red",
+    name: "red",
     hex: '#FF4136',
-    description: 'red is...'
+    description: 'a color at the end of the spectrum next to orange and opposite violet, as of blood, fire, or rubies'
   },
+  {
+    name: "orange",
+    hex: '#FF851B',
+    description: 'a bright reddish-yellow color like that of the skin of a ripe orange'
+  },
+  {
+    name: "yellow",
+    hex: '#FFDC00',
+    description: 'the color between green and orange in the spectrum, a primary subtractive color complementary to blue; colored like ripe lemons or egg yolks'
+  },
+  {
+    name: "green",
+    hex: '#2ECC40',
+    description: 'the color between blue and yellow in the spectrum; colored like grass or emeralds'
+  },
+  {
+    name: "blue",
+    hex: '#0074D9',
+    description: 'a color intermediate between green and violet, as of the sky or sea on a sunny day'
+  },
+  {
+    name: "violet",
+    hex: '#B10DC9',
+    description: 'a bluish-purple color seen at the end of the spectrum opposite red'
+  }
 ];
 
 //http://clrs.cc/
