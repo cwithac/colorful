b65fcc33db0e0ddfc3f2d68eea3d5443f2a41c69
Cathleen Wright <cathleenmwright@gmail.com>
2017-11-30 16:04:15 -0500

    Update PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Global Variables
 const express   		 = require('express');
 const app       		 = express();
-const port      		 = process.env.PORT || 3000;
+const PORT      		 = process.env.PORT || 3000;
 const mongoose  		 = require('mongoose');
 const bodyParser		 = require('body-parser');
 const methodOverride = require('method-override')
@@ -32,6 +32,6 @@ mongoose.connection.once('open', () => {
 	console.log('colorful app connected to mongo');
 });
 
-app.listen(port, () => {
-	console.log('colorful app connected to ' + port);
+app.listen(PORT, () => {
+	console.log('colorful app connected to ' + PORT);
 });
