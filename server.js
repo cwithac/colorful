6e97409b4e76f1ed1a092873f62f0bcfe26070e0
Cathleen Wright <cathleenmwright@gmail.com>
2017-11-30 19:08:13 -0500

    Remove body parser

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express   		 = require('express');
 const app       		 = express();
 const PORT      		 = process.env.PORT || 3000;
 const mongoose  		 = require('mongoose');
-// const bodyParser		 = require('body-parser');
 const methodOverride = require('method-override')
 
 // Middleware
