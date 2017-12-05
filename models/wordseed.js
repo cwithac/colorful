da7467b0adb0537a1b555946bb3f2127fed08abc
Cathleen Wright <cathleenmwright@gmail.com>
2017-12-05 16:53:39 -0500

    Seed data cleanup

---
+++ models/wordseed.js
@@ -0,0 +1,700 @@
+//COLOR IS REFLECTION OF DEVELOPER MONGO ID - MUST BE UPDATED FOR USE
+
+module.exports = [
+  {
+    name: "cherry",
+    hex: '#97110e',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "rose",
+    hex: '#df2933',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "jam",
+    hex: '#5f100f',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "merlot",
+    hex: '#53201d',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "garnet",
+    hex: '#5f0c08',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "crimson",
+    hex: '#b61317',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "ruby",
+    hex: '#8e090d',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "scarlet",
+    hex: '#8f0f11',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "wine",
+    hex: '#4b0807',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "brick",
+    hex: '#7d2916',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "apple",
+    hex: '#a71d17',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "mahogany",
+    hex: '#420d0b',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "sangria",
+    hex: '#5d1a16',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "berry",
+    hex: '#781916',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "currant",
+    hex: '#660d0c',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "brick",
+    hex: '#7d2916',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "blush",
+    hex: '#ba554c',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "candy",
+    hex: '#cf1916',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "lipstick",
+    hex: '#9a130f',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "pink",
+    hex: '#f49bcc',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "fuchsia",
+    hex: '#fa4ca9',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "punch",
+    hex: '#ef5579',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "watermelon",
+    hex: '#fc809d',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "flamingo",
+    hex: '#fca5b8',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "rogue",
+    hex: '#f06d8c',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "salmon",
+    hex: '#fcaba1',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "coral",
+    hex: '#fc7d6c',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "peach",
+    hex: '#f99486',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "strawberry",
+    hex: '#fa4e53',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "rosewood",
+    hex: '#9e4344',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "taffy",
+    hex: '#f888c4',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "ballet slipper",
+    hex: '#f49bbf',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "magenta",
+    hex: '#df2084',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "hot pink",
+    hex: '#f49bcc',
+    color: '5a26da64a749af5b6b5d1916'
+  },
+  {
+    name: "tangerine",
+    hex: '#f78237',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "cider",
+    hex: '#b3672f',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "rust",
+    hex: '#8a4010',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "ginger",
+    hex: '#ba5617',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "fire",
+    hex: '#db5729',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "cantaloupe",
+    hex: '#faa176',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "apricot",
+    hex: '#eb8227',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "clay",
+    hex: '#7e4014',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "honey",
+    hex: '#ea9627',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "marigold",
+    hex: '#fcad35',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "carrot",
+    hex: '#eb712a',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "squash",
+    hex: '#c75c1d',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "spice",
+    hex: '#79390d',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "marmalade",
+    hex: '#cf601b',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "amber",
+    hex: '#87320d',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "yam",
+    hex: '#ca5819',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "pumpkin",
+    hex: '#ff7619',
+    color: '5a26da64a749af5b6b5d1917'
+  },
+  {
+    name: "canary",
+    hex: '#f8c62e',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "gold",
+    hex: '#f7a529',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "daffodil",
+    hex: '#fded8d',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "flaxen",
+    hex: '#d5b661',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "butter",
+    hex: '#fde041',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "lemon",
+    hex: '#effc6b',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "mustard",
+    hex: '#e7b73b',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "corn",
+    hex: '#e3cb2e',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "medallion",
+    hex: '#e2b029',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "dandelion",
+    hex: '#fccc40',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "fire",
+    hex: '#fba42d',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "bumblebee",
+    hex: '#f8c62e',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "banana",
+    hex: '#fcf3a7',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "butterscotch",
+    hex: '#f9bc2d',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "dijon",
+    hex: '#c19120',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "honey",
+    hex: '#fec131',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "blonde",
+    hex: '#feea7d',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "pineapple",
+    hex: '#fde041',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "tuscan sun",
+    hex: '#fbcf40',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "macaroon",
+    hex: '#f7df7d',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "egg nog",
+    hex: '#f9e1a0',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "parmesan",
+    hex: '#fde897',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "sandcastle",
+    hex: '#d9c180',
+    color: '5a26da64a749af5b6b5d1918'
+  },
+  {
+    name: "slate",
+    hex: '#757b86',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "sky",
+    hex: '#66c5d9',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "navy",
+    hex: '#0d176f',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "indigo",
+    hex: '#28215c',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "cobalt",
+    hex: '#183eba',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "ocean",
+    hex: '#0b6064',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "azure",
+    hex: '#1828a3',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "cerulean",
+    hex: '#1893c0',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "lapis",
+    hex: '#2939bf',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "spruce",
+    hex: '#2d3f4c',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "aegean",
+    hex: '#20466d',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "blueberry",
+    hex: '#251a6e',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "denim",
+    hex: '#151f3d',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "admiral",
+    hex: '#091a91',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "sapphire",
+    hex: '#57b2bf',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "arctic",
+    hex: '#86edfc',
+    color: '5a26da64a749af5b6b5d191a'
+  },
+  {
+    name: "chartreuse",
+    hex: '#b2fa4b',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "juniper",
+    hex: '#3b5316',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "sage",
+    hex: '#738b6a',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "lime",
+    hex: '#aff164',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "fern",
+    hex: '#60ba67',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "olive",
+    hex: '#99bd69',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "emerald",
+    hex: '#12881d',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "pear",
+    hex: '#76b43a',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "moss",
+    hex: '#486c24',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "shamrock",
+    hex: '#1aaa24',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "seafoam",
+    hex: '#48ea99',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "pine",
+    hex: '#254f21',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "parakeet",
+    hex: '#1ebe51',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "seaweed",
+    hex: '#364a23',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "pickle",
+    hex: '#5a7c3a',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "pistachio",
+    hex: '#b3d2c2',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "basil",
+    hex: '#346030',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "crocodile",
+    hex: '#617c3f',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "teal",
+    hex: '#4daaac',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "jade",
+    hex: '#113711',
+    color: '5a26da64a749af5b6b5d1919'
+  },
+  {
+    name: "mauve",
+    hex: '#794c86',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "purple",
+    hex: '#a136c1',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "boysenberry",
+    hex: '#610736',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "lavender",
+    hex: '#e2a1f4',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "plum",
+    hex: '#5f1b37',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "magenta",
+    hex: '#9f0f59',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "lilac",
+    hex: '#b464ca',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "grape",
+    hex: '#653147',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "periwinkle",
+    hex: '#bc94d1',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "sangria",
+    hex: '#4d1029',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "iris",
+    hex: '#9769c2',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "heather",
+    hex: '#9a7db6',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "amethyst",
+    hex: '#a363e2',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "raisin",
+    hex: '#290916',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "orchid",
+    hex: '#ae6eeb',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "mulberry",
+    hex: '#4b0221',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "shadow",
+    hex: '#373737',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "graphite",
+    hex: '#584e5b',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "iron",
+    hex: '#322e31',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "pewter",
+    hex: '#6a697f',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "cloud",
+    hex: '#c6c6cf',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "silver",
+    hex: '#adadc6',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "smoke",
+    hex: '#59525e',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "anchor",
+    hex: '#42434c',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "ash",
+    hex: '#554c4d',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "porpoise",
+    hex: '#4d4d5c',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "dove",
+    hex: '#7c6e7e',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "charcoal",
+    hex: '#222023',
+    color: '5a26da64a749af5b6b5d191b'
+  },
+  {
+    name: "lead",
+    hex: '#40404d',
+    color: '5a26da64a749af5b6b5d191b'
+  }
+
+];
